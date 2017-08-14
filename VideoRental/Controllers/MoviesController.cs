9230fac4b025b29f84b3c87920669797f71d5525
Pankaj Vashishta <pankaj.vashishta@gmail.com>
2017-08-14 23:00:53 +1000

    BP-002  : Working solution

---
+++ VideoRental/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using VideoRental.Models;
+
+namespace VideoRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Titanic"};
+            return View(movie);
+        }
+    }
+}
